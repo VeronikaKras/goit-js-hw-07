699bbb2298d83f986b75ae3d8aba6d2b7b03b6f1
Veronika <nikakras.dance@gmail.com>
2023-07-23 16:29:27 +0300

    hw

--- js/task-07.js
+++
@@ -1,6 +0,0 @@
- const rangeInput = document.querySelector("#font-size-control");
-const textSpan = document.querySelector("#text");
-rangeInput.addEventListener("input", () => {
-  const fontSize = rangeInput.value + "px";
-  textSpan.style.fontSize = fontSize;
-});
